7df0d1a8b72f1c7e30e7c139c8c4c68acdd7b5d6
Atilla <atillacokyavuz1992@gmail.com>
2026-03-19 16:48:49 +0000

    File changes

--- src/pages/ReorderReview.jsx
+++ src/pages/ReorderReview.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import { Play, Pencil, Send } from 'lucide-react';
 
 const generateData = (windowDays, coverDays) => [
