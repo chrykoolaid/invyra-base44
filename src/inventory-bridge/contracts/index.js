4f6db7d30acdea4f7f7863f95486398ae4aeeb3e
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-25 19:48:09 +0800

    phase 5a disabled contract adapter

---
+++ src/inventory-bridge/contracts/index.js
@@ -0,0 +1,12 @@
+export {
+  INVENTORY_BRIDGE_CONTRACT_ADAPTER_COMPONENT,
+  INVENTORY_BRIDGE_CONTRACT_ADAPTER_PHASE,
+  INVENTORY_BRIDGE_EVENT_ENVELOPE_CLASSIFICATIONS,
+  assessInventoryBridgeEventEnvelopeContract,
+  classifyInventoryBridgeEventEnvelopeShape,
+  normalizeInventoryBridgeEventEnvelopeCandidate,
+} from './eventEnvelopeContract.js';
+
+export {
+  getInventoryBridgeEventEnvelopeContractDiagnostics,
+} from './eventEnvelopeDiagnostics.js';
