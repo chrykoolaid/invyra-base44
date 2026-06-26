9d0a6ba8914aa50f1dbd787b9367084e17c9d2f1
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-26 17:44:08 +0800

    feat(inventory-bridge): export disabled local transport scaffold

---
+++ src/inventory-bridge/localTransport/index.js
@@ -0,0 +1,17 @@
+export {
+  INVENTORY_BRIDGE_LOCAL_TRANSPORT_BLOCKERS,
+  INVENTORY_BRIDGE_LOCAL_TRANSPORT_COMPONENT,
+  INVENTORY_BRIDGE_LOCAL_TRANSPORT_FIXTURES,
+  INVENTORY_BRIDGE_LOCAL_TRANSPORT_PHASE,
+  INVENTORY_BRIDGE_LOCAL_TRANSPORT_STATUSES,
+} from './localTransportFixtures.js';
+
+export {
+  buildInventoryBridgeLocalTransportPreflight,
+  getInventoryBridgeLocalTransportPreflightResults,
+  projectInventoryBridgeLocalTransportPreflightResult,
+} from './localTransportPreflight.js';
+
+export {
+  getInventoryBridgeLocalTransportDiagnostics,
+} from './localTransportDiagnostics.js';
