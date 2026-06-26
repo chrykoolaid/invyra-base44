735a67a9b4d34267cc2b1f4abdb9081822ae0b45
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-26 17:57:47 +0800

    feat(inventory-bridge): export Phase 7 handshake preparation

---
+++ src/inventory-bridge/trainingHandshake/index.js
@@ -0,0 +1,17 @@
+export {
+  INVENTORY_BRIDGE_TRAINING_HANDSHAKE_BLOCKERS,
+  INVENTORY_BRIDGE_TRAINING_HANDSHAKE_COMPONENT,
+  INVENTORY_BRIDGE_TRAINING_HANDSHAKE_FIXTURES,
+  INVENTORY_BRIDGE_TRAINING_HANDSHAKE_PHASE,
+  INVENTORY_BRIDGE_TRAINING_HANDSHAKE_STATUSES,
+} from './trainingHandshakeFixtures.js';
+
+export {
+  buildInventoryBridgeTrainingHandshakeReadiness,
+  getInventoryBridgeTrainingHandshakeReadinessResults,
+  projectInventoryBridgeTrainingHandshakeReadinessResult,
+} from './trainingHandshakeReadiness.js';
+
+export {
+  getInventoryBridgeTrainingHandshakeDiagnostics,
+} from './trainingHandshakeDiagnostics.js';
