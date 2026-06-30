36ef38c2425a26e0959ddf3717ed8c2e7339207d
chrykoolaid <atillacokyavuz1992@gmail.com>
2026-06-30 21:46:47 +0800

    Export local bridge service foundation

---
+++ src/inventory-bridge/localBridgeService/index.js
@@ -0,0 +1,14 @@
+export {
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_COMPONENT,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_EVENT_TYPES,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_OPERATION_TYPES,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_PHASE,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_RECEIPT_STATUSES,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_SERVICE_NAME,
+  INVENTORY_DESKTOP_LOCAL_BRIDGE_VERSION,
+  createInventoryDesktopLocalBridgeService,
+  getInventoryDesktopLocalBridgeHealth,
+  getInventoryDesktopLocalBridgeServiceDiagnostics,
+  getInventoryDesktopLocalBridgeServiceStatus,
+  handleInventoryDesktopLocalBridgeHandoff,
+} from './localBridgeService.js';
